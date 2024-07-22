fd92e769abaaa60f25a156c713606ea05418b31c
ZeroXashu <ashuroy761@gmail.com>
2024-07-22 21:36:42 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(env.get("API_ID"))
-    API_HASH = str(env.get("API_HASH"))
+    API_ID = int(env.get("17584090"))
+    API_HASH = str(env.get("3f779654fdd3b9278fd95869aabf4593"))
     BOT_TOKEN = str(env.get("BOT_TOKEN"))
-    OWNER_ID = int(env.get('OWNER_ID', ''))
+    OWNER_ID = int(env.get('OWNER_ID', '7252430326'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
-    SESSION_NAME = str(env.get('SESSION_NAME', 'LDC_FILE_TO_LINK_BOT'))
+    DATABASE_URL = str(env.get('mongodb+srv://Aaki8490:vnCMn6F5l8nqXxFe@cluster0.jlgdebe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "luxf2link"))
+    SESSION_NAME = str(env.get('SESSION_NAME', 'LuxFiletolinkbot'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', False)
     FORCE_SUB = True if str(FORCE_SUB).lower() == "true" else False
